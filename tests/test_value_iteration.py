fixed error in valid state computation and added some unittests

tests/test_value_iteration.py
@@ -181,8 +181,111 @@ class TestUtils(unittest.TestCase):
         expected_positions = [(0, 1), (1, 0), (1, 1), (1, 2), (2, 1)]
         self.assertEqual(available_positions, expected_positions)
     
+    def test_is_valid_state_empty_board(self):
+        board = np.zeros((3, 3), dtype=int)
+        valid = ttt._is_valid_state(board)
+        expected = True
+        self.assertEqual(valid, expected)
 
-    
+    def test_is_valid_too_large_board(self):
+        board = np.zeros((4, 4), dtype=int)
+        with self.assertRaises(AssertionError):
+            ttt._is_valid_state(board)
+
+    def test_is_valid_x_ahead_y_1(self):
+        board = np.array([[0, 0, 0],
+                          [0, 1, 0],
+                          [0, 0, 0]])
+        valid = ttt._is_valid_state(board)
+        expected = True
+        self.assertEqual(valid, expected)
+
+    def test_is_valid_x_ahead_y_2(self):
+        board = np.array([[0, 0, 0],
+                          [0, 1, -1],
+                          [0, 0, 1]])
+        valid = ttt._is_valid_state(board)
+        expected = True
+        self.assertEqual(valid, expected)
+
+    def test_is_valid_x_draw_y_1(self):
+        board = np.array([[0, 0, 0],
+                          [0, 1, -1],
+                          [0, 0, 0]])
+        valid = ttt._is_valid_state(board)
+        expected = True
+        self.assertEqual(valid, expected)
+
+    def test_is_valid_x_draw_y_2(self):
+        board = np.array([[1, -1, 1],
+                          [-1, 1, -1],
+                          [1, -1, 0]])
+        valid = ttt._is_valid_state(board)
+        expected = True
+        self.assertEqual(valid, expected)
+
+    def test_is_valid_x_winner(self):
+        board = np.array([[1, -1, 0],
+                          [0, 1, -1],
+                          [0, 0, 1]])
+        valid = ttt._is_valid_state(board)
+        expected = True
+        self.assertEqual(valid, expected)
+
+    def test_is_valid_y_winner(self):
+        board = np.array([[1, -1, -1],
+                          [0, 1, -1],
+                          [1, 1, -1]])
+        valid = ttt._is_valid_state(board)
+        expected = True
+        self.assertEqual(valid, expected)
+
+    def test_is_valid_draw(self):
+        board = np.array([[1, -1, -1],
+                          [-1, 1, 1],
+                          [1, 1, -1]])
+        valid = ttt._is_valid_state(board)
+        expected = True
+        self.assertEqual(valid, expected)
+
+    def test_is_valid_wrong_symbols(self):
+        board = np.array([[1, -1, -1],
+                          [2, 1, -1],
+                          [1, 1, -1]])
+        with self.assertRaises(AssertionError):
+            ttt._is_valid_state(board)
+
+    def test_is_invalid_too_much_x_1(self):
+        board = np.array([[1, 1, 1],
+                          [1, 1, 1],
+                          [1, 1, 1]])
+        valid = ttt._is_valid_state(board)
+        expected = False
+        self.assertEqual(valid, expected)
+
+    def test_is_invalid_too_much_x_2(self):
+        board = np.array([[1, 1, 0],
+                          [0, 0, 0],
+                          [0, 0, 0]])
+        valid = ttt._is_valid_state(board)
+        expected = False
+        self.assertEqual(valid, expected)
+
+    def test_is_invalid_too_much_y_1(self):
+        board = np.array([[-1, -1, 1],
+                          [-1, -1, 1],
+                          [-1, -1, 1]])
+        valid = ttt._is_valid_state(board)
+        expected = False
+        self.assertEqual(valid, expected)
+
+    def test_is_invalid_too_much_y_2(self):
+        board = np.array([[-1, -1, 0],
+                          [0, 0, 0],
+                          [0, 0, 1]])
+        valid = ttt._is_valid_state(board)
+        expected = False
+        self.assertEqual(valid, expected)
 
 
 if __name__ == "__main__":

tic_tac_toe/tic_tac_toe_Value_Iteration.py
@@ -49,11 +49,24 @@ def _get_available_positions(board):
         return list(zip(*np.where(np.array(board).reshape(3,3) == PlayerSymbol.EMPTY)))
 
 
-def _is_valid_state(state):
-    board = np.array(state)
+def _is_valid_state(board):
+    """
+    Hard wired who goes first -> we might come up with something better at some point
+    valid states assuming X starts first:
+        - empty
+        - same number of X and Y
+        - 1 X more than Y --> X has won the game
+        - only X, Y and EMPTY symbols on the board
+    """
+    assert board.shape == (3, 3)
     x_count = np.sum(board == PlayerSymbol.X)
     y_count = np.sum(board == PlayerSymbol.Y)
-    return not (y_count > x_count or x_count - y_count > 1)
+    empty_count = np.sum(board == PlayerSymbol.EMPTY)
+
+    # assertion is better -> if we add weird symbols, than the whole game is corrupt
+    assert x_count + y_count + empty_count == 9
+
+    return (x_count == y_count or x_count - y_count == 1)
 
 
 def _is_full(state):