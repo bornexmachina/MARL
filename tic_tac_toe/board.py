fc8a1c1cc4679d7b478c4d79ee2b20a25299d04c
bornexmachina <aborn.priv@gmail.com>
2025-08-25 22:27:30 +0200

    started refactoring
    too much code in one single file --> gonna be utils, board and player
    
    have seen some --> mdp, game, ... but I think it is too much

---
+++ tic_tac_toe/board.py
@@ -0,0 +1,38 @@
+from value_iteration_maxmin import PlayerSymbol
+
+
+class Board:
+    def __init__(self, player_1, player_2):
+        self.board = np.zeros((3, 3), dtype=int)
+        self.has_ended = False
+        self.player_1 = player_1
+        self.player_2 = player_2
+        self.current_player = PlayerSymbol.X
+    
+    def switch_players(self):
+        self.current_player = PlayerSymbol.X if self.current_player == PlayerSymbol.Y else PlayerSymbol.Y
+    
+    def update_state(self, action):
+        if action is None:
+            raise ValueError("Action is not allowed!")
+        if action in _get_available_positions(self.board):
+            self.board[action] = self.current_player
+            winner = _check_winner(self.board)
+
+            if winner is not None:
+                self.has_ended = True
+                _print_board(self.board)
+
+                if winner == PlayerSymbol.EMPTY:
+                    print("Draw!")
+                else:
+                    print(f"Winner is Player {_get_winner_name(winner)}!")
+
+            self.switch_players()
+
+    def play(self):
+        while not self.has_ended:
+            player = self.player_1 if self.current_player == PlayerSymbol.X else self.player_2
+            action = player.choose_action(self.board)
+            self.update_state(action)
+            _print_board(self.board)
