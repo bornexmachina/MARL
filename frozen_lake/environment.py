bdc436d714e9e12f16dc53b3e7f8668d0556915d
bornexmachina <aborn.priv@gmail.com>
2025-09-02 22:27:03 +0200

    trajectory plotter
    took ages to think about the transformation from the array idx to plot coordinates --> proud to do it without tech overlords

--- frozen_lake/environment.py
+++ frozen_lake/environment.py
@@ -31,19 +31,39 @@ class Environment:
         Initial configuration is from the blog
             --> lets see whether tehe results are the same
         """
-        self.lake = [[LakeState.SAFE, LakeState.FROZEN, LakeState.FROZEN, LakeState.FROZEN],
-                    [LakeState.FROZEN, LakeState.HOLE, LakeState.FROZEN, LakeState.HOLE],
-                    [LakeState.FROZEN, LakeState.FROZEN, LakeState.FROZEN, LakeState.HOLE],
-                    [LakeState.HOLE, LakeState.FROZEN, LakeState.FROZEN, LakeState.GOAL]]
+        self.lake = np.array([[LakeState.SAFE, LakeState.FROZEN, LakeState.FROZEN, LakeState.FROZEN],
+                              [LakeState.FROZEN, LakeState.HOLE, LakeState.FROZEN, LakeState.HOLE],
+                              [LakeState.FROZEN, LakeState.FROZEN, LakeState.FROZEN, LakeState.HOLE],
+                              [LakeState.HOLE, LakeState.FROZEN, LakeState.FROZEN, LakeState.GOAL]])
         
-    def visualize(self):
+    def visualize_initial_env(self):
         data = np.array(self.env)
         custom_colors = ["white", "lightsteelblue", "darkslateblue", "gold"]
         cmap = ListedColormap(custom_colors)
-        plt.imshow(data, cmap=cmap, interpolation='nearest')
-        plt.xticks([])
-        plt.yticks([])
-        plt.show()
+
+        fig, ax = plt.subplots(figsize=(4, 4))
+        
+        ax.imshow(data, cmap=cmap, origin="upper", interpolation="nearest", extent=(0, 4, 0, 4))
+        ax.grid(color="lightgrey", linewidth=0.5)
+        ax.set_xticks(np.arange(0, 5, 1))
+        ax.set_yticks(np.arange(0, 5, 1))
+        
+        return ax
 
     def visualize_trajectory(self, trajectory):
-        pass
+        """
+        trajectory is a list of tuples. each tuple is x,y coordinate of the lake
+        --> we need a transformation from our indexing of the array to actual plot
+        """
+        ax = self.visualize_initial_env()
+        offset = 0.5
+        for i, point in enumerate(trajectory):
+            color = "lightgrey"
+            if i == len(trajectory) - 1:
+                color = "orangered"
+            x = point[1] + offset
+            y = len(self.lake) - 1 - point[0] + offset
+            
+            ax.scatter(x, y, color=color, s=50, marker="D")
+
+        plt.show()
