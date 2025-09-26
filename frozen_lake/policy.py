00ba6230a4b426b90b839723ebc3a546c67cd1c2
bornexmachina <aborn.priv@gmail.com>
2025-09-26 23:26:14 +0200

    started softmax implementation

--- frozen_lake/policy.py
+++ frozen_lake/policy.py
@@ -1,6 +1,22 @@
 #
 # differentiable policy class --> something to take actions in 2D
 #
+import numpy as np
 
-class Policy:
-    pass
+class SoftmaxPolicy:
+    def __init__(self):
+        pass
+
+    def softmax(self, logits):
+        exp_scores = np.exp(logits - np.max(logits, axis=1, keepdims=True))
+        return exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
+    
+    def cross_entropy_loss(self, probabilities, Y):
+        n_samples = Y.shape[0]
+
+        log_likelihood = -np.log(probabilities[range(n_samples), np.argmax(Y, axis=1)])
+        return np.sum(log_likelihood, axis=1) / n_samples
+    
+    def derivative_single_sample(self, logits):
+        p = self.softmax(logits)
+        return np.diag(p) - np.outer(p, p)
