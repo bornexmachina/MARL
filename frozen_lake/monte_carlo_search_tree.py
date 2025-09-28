9645837f7e35a82d0a954b9e9f91bf309f78d087
bornexmachina <aborn.priv@gmail.com>
2025-09-28 23:10:15 +0200

    started working on monte carlo search tree

---
+++ frozen_lake/monte_carlo_search_tree.py
@@ -0,0 +1,10 @@
+# implement monte carlo search tree used in RL
+# we follow the implementation of gibberblot
+# we will need a node for the trees
+# and MCTS itself
+
+class Node:
+    pass
+
+class MCTS:
+    pass
