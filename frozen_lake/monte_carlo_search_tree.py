cff30fa7c049a5017d9296a53d67c604e19d7929
bornexmachina <aborn.priv@gmail.com>
2025-09-30 22:13:57 +0200

    further work on MCST

--- frozen_lake/monte_carlo_search_tree.py
+++ frozen_lake/monte_carlo_search_tree.py
@@ -20,10 +20,23 @@ class Node:
         pass
 
 class MCTS:
+    """
+    1) Q-value function approximation
+    2) ExpectiMax search tree
+    3) Computational budget as hard limit
+    4) Best performing action within the budget
+    """
     def __init__(self):
         pass
 
     def mcts(self):
+        """
+        while current_time < T do:
+            selected_node <- Select(s_0)
+            child <- Expand(selected_node)
+            G <- Simulate(child)
+            Backpropagate(selected_node, child, Q, G)
+        """
         pass
 
     def create_root_node(self):
